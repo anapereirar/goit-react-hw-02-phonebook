ecfd53d9d8cacbe8efa7bb1bb6dcf52c7b56107c
Ana <ana.pereirar@utadeo.edu.co>
2023-07-24 00:02:26 -0500

    Tarea2

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { Component } from "react";
+import { nanoid } from "nanoid";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import Filter from "./components/Filter/Filter";
+import "./App.css";
+
+class App extends Component {
+  
+  state = {
+    contacts: [
+      {id: nanoid(), name: 'Rosie Simpson', number: '459-12-56'},
+      {id: nanoid(), name: 'Hermione Kline', number: '443-89-12'},
+      {id: nanoid(), name: 'Eden Clements', number: '645-17-79'},
+      {id: nanoid(), name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
+    // name: '',
+    // number: ''
+  }
+
+  formSubmitHandler = data => {
+    this.repeatControl(data);
+  };
+
+  repeatControl = data => {
+    let nameArray = [];
+    nameArray = this.state.contacts.map(cur => cur.name);
+    if (!nameArray.includes(data.name)) {
+      let arrayCont = [];
+      arrayCont = [
+        ...this.state.contacts,
+        { id: nanoid(), name: data.name, number: data.number },
+      ];
+      return this.setState({ ...this.state, contacts: arrayCont });
+    } else {
+      alert(`${data.name} is already in contacts`);
+    }
+  };
+  
+  elementDelete = (arr, idContact) => {
+    let newArr = arr.filter(elem => elem.id !== idContact);
+    return newArr;
+  };
+
+  deleteContactFromContactList = idContact => {
+    let newArrAfterDel = this.elementDelete(this.state.contacts, idContact);
+    this.setState({
+      ...this.state,
+      contacts: [...newArrAfterDel],
+    });
+  };
+
+  setFilterToState = filterData => {
+    this.setState({ ...this.state, filter: `${filterData}` });
+  };
+
+  filterArr = fArr => {
+    let newArr = fArr.filter(cur =>
+      cur.name.toUpperCase().includes(this.state.filter),
+    );
+    return newArr;
+  };
+ 
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmitData={this.formSubmitHandler} />
+        <h2>Contacts</h2>
+        <Filter setFilterToState={this.setFilterToState}/>
+        <ContactList contacts={this.filterArr(this.state.contacts)} del={this.deleteContactFromContactList}/>
+      </div>
+    );
+  }
+}
+
+export default App;
